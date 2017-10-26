b3ae2d65425713524c1dbb7109d88bdc703583f4
Shenglong Li <shenglong.li@outlook.com>
2017-10-26 14:09:56 -0700

    Create empty react app

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+ReactDOM.render(
+  <div />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();

c9aaf0fe664cdfc033a1b16ee9d311dd2b31ff2a
Shenglong Li <shenglol@microsoft.com>
2018-05-01 13:14:25 -0700

    Renaming

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,2 +1,2 @@
 export * from './schemaStore';
-export * from './TemplateStore';
+export * from './templateStore';
--- src/theming/index.ts
+++ src/theming/index.ts
@@ -1,2 +1,2 @@
 export { default as themes } from './themes';
-export * from './styled-components';
+export * from './styledComponents';
